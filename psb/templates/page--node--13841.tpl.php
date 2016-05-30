871dbe9685096dfe14b8741f37422cb85133ee15
Brett <brett.thomson@ipaustralia.gov.au>
2016-05-31 09:42:01 +1000

    Templates for landing pages

---
+++ psb/templates/page--node--13841.tpl.php
@@ -0,0 +1,34 @@
+<?php include DRUPAL_ROOT . "/" . drupal_get_path('theme', 'psb') . "/templates/includes/header.inc"; ?>
+
+<div class="main-container">
+
+  <div class="overlay" />
+
+  <?php if (!empty($messages)): ?>
+    <div class="messages">
+      <div class="container">
+        <?php print $messages; ?>
+      </div>
+    </div>
+  <?php endif; ?>
+
+  <div>
+
+    <section>
+      <a id="main-content"></a>
+      <?php if (!empty($tabs)): ?>
+        <?php print render($tabs); ?>
+      <?php endif; ?>
+      <?php if (!empty($page['help'])): ?>
+        <?php print render($page['help']); ?>
+      <?php endif; ?>
+      <?php if (!empty($action_links)): ?>
+        <ul class="action-links"><?php print render($action_links); ?></ul>
+      <?php endif; ?>
+      <?php print render($page['content']); ?>
+    </section>
+
+  </div>
+</div>
+
+<?php include DRUPAL_ROOT . "/" . drupal_get_path('theme', 'psb') . "/templates/includes/footer.inc"; ?>
