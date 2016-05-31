7849620711a2bcdfba58b6193943961984ef4618
josh <josh@pianca.com.au>
2016-05-31 13:23:59 +1000

    recompile sass

--- psb/templates/block--block--1.tpl.php
+++ psb/templates/block--block--1.tpl.php
@@ -6,7 +6,7 @@
   <li><a href="/attorney-registration/incorporation">How to register as an incorporated attorney&nbsp;&nbsp;<i class="fa fa-arrow-right" aria-hidden="true"></i></a></li>
   <li><a href="/attorney-registration/accredited-courses">Accredited courses&nbsp;&nbsp;<i class="fa fa-arrow-right" aria-hidden="true"></i></a></li>
 </ul>
-<h6>The Professional Standards Board for Patent and Trade Marks Attorneys (PSB) is a statutory body established under section 227A of the <em>Patents Act 1990</em> and constituded under the <em>Patents Regulations 1991</em>. We are responsible for administering the regulatory and disciplinary regimes for patent and trade marks attorneys in Australia.</h6>
+<h6>The Professional Standards Board for Patent and Trade Marks Attorneys (PSB) is a statutory body established under section 227A of the <em>Patents Act 1990</em> and constituted under the <em>Patents Regulations 1991</em>. We are responsible for administering the regulatory and disciplinary regimes for patent and trade marks attorneys in Australia.</h6>
 <br />
 <br />
 <a href="node/261" id="mre">Find out more&nbsp;&nbsp;<i class="fa fa-arrow-right" aria-hidden="true"></i></a>
