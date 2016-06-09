2f7f7596fdc374439fb9ae7aa03e6a96e858e635
josh <josh@pianca.com.au>
2016-06-10 08:58:36 +1000

    jeff update

--- psb/templates/block--block--21.tpl.php
+++ psb/templates/block--block--21.tpl.php
@@ -1,7 +1,16 @@
 <section id="block-block-21">
-<div class="container">
-<h2>Trans-Tasman attorney regime</h2>
-<p>Subject to the passing of appropriate legislation by the Australian and New Zealand governments, patent attorneys in Australia and New Zealand will soon be covered by a single-attorney registration system.</p>
-<br /><a href="/node/13786" id="mre">Read more</a>
-</div>
+	<div class="row" style="margin-bottom:1%;">
+		<div class="col-md-8">
+			<h2>Trans-Tasman attorney regime</h2>
+			<p>Subject to the passing of appropriate legislation by the Australian and New Zealand governments, patent attorneys in Australia and New Zealand will soon be covered by a single-attorney registration system.</p>
+			<br /><a href="/node/13786" id="mre">Read more</a>
+		</div>
+	</div>
+	<div class="row"  style="margin-bottom:0;">
+		<div class="col-md-8">
+			<h2>Upcoming board vacancies</h2>
+			<p>Expressions of interest are invited from persons who wish to be considered to serve on the Board. There will be at least two vacancies occurring in October 2016. The Australian Government is seeking individuals with a sound experience in IP protection, IP law, statutory interpretation, administrative law, or relevant academic employment to fill the vacancies.</p>
+			<p>Registered patent and/or trade marks attorneys with at least 5 years of post-registration experience, and academics with a particular interest in IP law and policy, or those with sound administrative law skills are strongly encouraged to apply.</p>
+			<br /><a href="/file/746" id="mre">Read more</a>
+		</div>
 </section>
