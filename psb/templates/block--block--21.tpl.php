b28b66fab2e2ff2d2a7743f1a00e05ff76aca783
Brett <brett.thomson@ipaustralia.gov.au>
2016-08-01 14:11:39 +1000

    Removing content as requested by Kathleen

--- psb/templates/block--block--21.tpl.php
+++ psb/templates/block--block--21.tpl.php
@@ -6,12 +6,4 @@
 			<br /><a href="/node/13786" id="mre">Read more</a>
 		</div>
 	</div>
-	<div class="row"  style="margin-bottom:0;">
-		<div class="col-md-8">
-			<hr />
-			<h2>Upcoming board vacancies</h2>
-			<p>Expressions of interest are invited from persons who wish to be considered to serve on the Board. There will be at least two vacancies occurring in October 2016. The Australian Government is seeking individuals with a sound experience in IP protection, IP law, statutory interpretation, administrative law, or relevant academic employment to fill the vacancies.</p>
-			<p>Registered patent and/or trade marks attorneys with at least 5 years of post-registration experience, and academics with a particular interest in IP law and policy, or those with sound administrative law skills are strongly encouraged to apply.</p>
-			<br /><a href="/file/746" id="mre">Read more</a>
-		</div>
 </section>
